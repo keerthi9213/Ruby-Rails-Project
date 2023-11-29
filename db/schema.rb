62d3251158225f8c8365ceb5540d4160a9527260
jashwanth0011 <jashwanthreddy.vgf196@gmail.com>
2023-11-29 13:14:09 -0600

    Us13 changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_25_193454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "inventory_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
   add_foreign_key "cart_items", "birdhouses"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "carts", "users"
+  add_foreign_key "inventories", "users"
+  add_foreign_key "questions", "users"
 end
