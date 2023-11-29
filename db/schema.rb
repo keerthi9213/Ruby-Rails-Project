f98bcc8430cbf1fa83cf53dda9024e03f0a4cdf3
sddhpala <sddhpala@memphis.edu>
2023-11-29 10:30:18 -0600

    User story 17 about Us

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_25_193454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "inventory_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,9 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_235333) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "inventories", "users"
 end
