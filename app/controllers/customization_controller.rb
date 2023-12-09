805f00f47f42371919936157f16b68f6ef67e885
Keerthi Reddy <keerthireddy9213@gmail.com>
2023-12-09 12:58:01 -0600

    Customization changes

--- app/controllers/customization_controller.rb
+++ app/controllers/customization_controller.rb
@@ -1,29 +1,46 @@
 class CustomizationController < ApplicationController
+    before_action :set_birdhouses_and_themes, only: [:index, :create]  # Re-use for both actions
+    
     def index
-        @birdhouses = Birdhouse.all
-        # Define themes or fetch them if they are dynamically generated
-        @themes = ['Spring Blossom', 'Autumn Twilight', 'Winter Wonderland', 'Summer Solstice']
+      # @birdhouses and @themes are set via the before_action
+    end
+  
+    def create1
+      @customization = Customization.new(customization_params)
+      @customization.user = current_user # Assuming Devise for user authentication
+  
+      if @customization.save
+        flash[:notice] = 'Customization saved successfully!'
+        redirect_to customization_path
+      else
+        flash.now[:alert] = 'There was an error saving your customization.'
+        render :index
+      end
     end
 
     def create
-        # Assuming Customization model has user_id, birdhouse_id, theme, and design_details attributes
         @customization = Customization.new(customization_params)
-        @customization.user = current_user # Assuming Devise for user authentication
-    
+        @customization.user = current_user
+      
         if @customization.save
           flash[:notice] = 'Customization saved successfully!'
           redirect_to customization_path
         else
-          flash[:alert] = 'There was an error saving your customization.'
+          flash.now[:alert] = 'There was an error saving your customization: ' + @customization.errors.full_messages.to_sentence
           render :index
         end
-    end
+      end
+      
     
     private
-
-    def customization_params
-        params.require(:customization).permit(:user_id, :birdhouse_id, :theme, :design_details)
+  
+    def set_birdhouses_and_themes
+      @birdhouses = Birdhouse.all
+      @themes = ['Spring Blossom', 'Autumn Twilight', 'Winter Wonderland', 'Summer Solstice']
     end
-    
-      
-end
+  
+    def customization_params
+      params.require(:customization).permit(:birdhouse_id, :theme, :design_details)
+    end
+  end
+  
