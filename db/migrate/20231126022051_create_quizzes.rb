85f244033ab14dedef3798702a0d5c89868515de
jashwanth0011 <jashwanthreddy.vgf196@gmail.com>
2023-12-09 13:49:35 -0600

    View quiz results

--- db/migrate/20231126022051_create_quizzes.rb
+++ db/migrate/20231126022051_create_quizzes.rb
@@ -1,10 +1,11 @@
 class CreateQuizzes < ActiveRecord::Migration[7.0]
   def change
     create_table :quizzes do |t|
+      t.string :city
       t.string :temperature
       t.integer :number_of_birds
       t.integer :number_of_species
-      # Add other fields as necessary
+      t.text :species
 
       t.timestamps
     end
