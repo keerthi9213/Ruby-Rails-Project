a141c8e28ccf1db2ebf0031904c7d0aef2a60f62
Tejaswi Kanneganti <tejaswikanneganti20@gmail.com>
2023-12-10 16:59:01 -0600

    Add Listings

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,42 +1,23 @@
 class InventoryController < ApplicationController
-    def new
+  def new
         @birdhouse = Birdhouse.new
         render :new
-    end
+  end
 
-    def save
-        @birdhouse = Birdhouse.new(birdhouse_params)
-        if @birdhouse.build(birdhouse_params)
-          redirect_to listings_path(@birdhouse), notice: 'Birdhouse was successfully created.'
-        else
-          render :new
-        end
-    end
-
-    def birdhouse_params
-        # Permit your birdhouse parameters here, for example:
-        params.require(:birdhouse).permit(:figure_image, :material, :color, :style, :roof_design, :size, :artistry, :quantity)
-    end
-
-
-    def save1
-        @birdhouse = Birdhouse.new(birdhouse_params)
+  def save
+      @birdhouse = Birdhouse.new(birdhouse_params)
       
-        if @birdhouse.save
-          redirect_to listings_path(@birdhouse), notice: 'Birdhouse was successfully created.'
-        else
-          render :new
-        end
-    end
+      if @birdhouse.save
+         redirect_to listings_show_path(@birdhouse), notice: 'Birdhouse was successfully created.'
+      else
+         render :new
+      end
+  end
 
-    def save2
-        @birdhouse = Birdhouse.create(birdhouse_params)
-        if @birdhouse.persisted?
-          redirect_to listings_path(@birdhouse), notice: 'Birdhouse was successfully created.'
-        else
-          render :new
-        end
-    end
+  def birdhouse_params
+      # Permit your birdhouse parameters here, for example:
+      params.require(:birdhouse).permit(:figure_image, :material, :color, :style, :roof_design, :size, :artistry, :quantity, :price)
+  end
       
       
       
