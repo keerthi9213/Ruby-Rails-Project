68d8fa6dd7633bd1437314a2d0655cac4e6862ea
Tejaswi Kanneganti <tejaswikanneganti20@gmail.com>
2023-12-12 17:25:07 -0600

    Updating main

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :orders, dependent: :destroy
-  has_many :orders
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def vendor?
