de06c31442cc43bfb0064a0260171120cbf143cc
Keerthi Reddy <keerthireddy9213@gmail.com>
2023-12-11 18:14:45 -0600

    Customization user admin interaction

--- app/controllers/customization_controller.rb
+++ app/controllers/customization_controller.rb
@@ -1,14 +1,6 @@
 class CustomizationController < ApplicationController
     before_action :authenticate_user!, only: [:index, :create]  # Re-use for both actions
     
-    
-    def index1
-      # @birdhouses and @themes are set via the before_action
-      @customizations = current_user.customizations
-      @customizations = Customization.all if current_user.role?# This assumes the user has_many :customizations
-    # For admins, you might want to show all customizations:
-      
-    end
 
     def index
       @birdhouses = Birdhouse.all # This line will set @birdhouses for the view
@@ -16,15 +8,13 @@ class CustomizationController < ApplicationController
       @customizations = Customization.all if current_user.role? # Ensure this line correctly checks for admin role
     end
     
-  
    
-
     def create
         @customization = Customization.new(customization_params)
         @customization.user = current_user
       
         if @customization.save
-          flash[:notice] = 'Customization saved successfully!'
+          flash[:notice] = 'Customization saved successfully. We will get back to you within 2-3 working days.'
           redirect_to customization_path
         else
           @customizations = current_user.customizations 
@@ -36,7 +26,6 @@ class CustomizationController < ApplicationController
     
     private
   
-    
   
     def customization_params
       params.require(:customization).permit(:birdhouse_id, :figure_image, :design_details)
